08faebf3860f3461b242d1a606d176d64e7efd6a
Amir Moulavi <amir.moulavi@gmail.com>
2011-07-25 19:45:25 +0200

    New tests are added to cover a case in LeastCPULoadAlgorithm

--- src/test/scala/cloud/elb/LeastCPULoadAlgorithmSpec.scala
+++ src/test/scala/cloud/elb/LeastCPULoadAlgorithmSpec.scala
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 import instance.Node
+import java.util.{List => JList}
 
 
 @RunWith(classOf[JUnitRunner])
@@ -37,6 +38,30 @@ class LeastCPULoadAlgorithmSpec extends FlatSpec with ShouldMatchers with ELBTes
     cpuLoad(List(3.0, 3.0, 56.0, 45.0, 62.0))
     sentRequests(List(10, 1, 120, 140, 160))
     algorithm.getNextNodeFrom(replicas) should equal (node2)
+  }
+
+  "algorithm" should "select the nodes with least cpu loads to be removed" in {
+    algorithm.clear
+    cpuLoad(List(3.0, 0.0, 34.2, 45.0, 62.0))
+    sentRequests(List(10, 1, 120, 140, 160))
+    val result = algorithm.selectNodesToRemove(replicas, 3)
+    result.size() should equal (3)
+    result.get(0) should equal (node2)
+    result.get(1) should equal (node1)
+    result.get(2) should equal (node3)
+  }
+
+  "algorithm" should "select all the members when the request number of nodes to be removed is greater than the actual number of nodes" in {
+    algorithm.clear
+    cpuLoad(List(3.0, 0.0, 34.2, 45.0, 62.0))
+    sentRequests(List(10, 1, 120, 140, 160))
+    val result = algorithm.selectNodesToRemove(replicas, 7)
+    result.size() should equal (5)
+    result.get(0) should equal (node2)
+    result.get(1) should equal (node1)
+    result.get(2) should equal (node3)
+    result.get(3) should equal (node4)
+    result.get(4) should equal (node5)
   }
 
   def sentRequests(list:List[Int]) = {
