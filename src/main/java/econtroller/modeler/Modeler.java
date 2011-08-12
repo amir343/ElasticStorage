057584df410ff4d6928aad7da74effc48c62d41d
Amir Moulavi <amir.moulavi@gmail.com>
2011-08-12 13:48:28 +0200

    Implementation of SLA requirements and calculation

--- src/main/java/econtroller/modeler/Modeler.java
+++ src/main/java/econtroller/modeler/Modeler.java
@@ -184,6 +184,7 @@ public class Modeler extends ComponentDefinition {
                             event.getThroughputMean(),
                             event.getNrNodes()
             );
+            logger.warn("CPULoadViolation: %" + event.getCpuLoadViolation() + ", ResponseTimeViolation: %" + event.getResponseTimeViolation());
         }
     };
 
