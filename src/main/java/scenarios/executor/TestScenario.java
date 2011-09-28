948fc16adc8c022e78dac6accc6e42bc718339d9
Amir Moulavi <amir.moulavi@gmail.com>
2011-09-28 22:52:06 +0200

    Apache Copyright header is added to all source files

--- src/main/java/scenarios/executor/TestScenario.java
+++ src/main/java/scenarios/executor/TestScenario.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2011 Amir Moulavi (amir.moulavi@gmail.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package scenarios.executor;
 
 import cloud.CloudProvider;
@@ -22,7 +37,9 @@ public class TestScenario {
     	Cloud cloud = new Cloud(CloudProvider.class, Instance.class) {
     		{
     			cloudProviderAddress("127.0.0.1", 23444);
+/*
                 headless();
+*/
 //    			node("node1", "127.0.0.1", 23445)
 //    				.cpu(2.2)
 //    				.memoryGB(8)
